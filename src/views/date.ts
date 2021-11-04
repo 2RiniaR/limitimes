245829a6154782d59c87e0262c1c9e2efb2058b9
watano1168 <watano1168@gmail.com>
2021-11-05 02:46:17 +0900

    メッセージ引用のEmbedに、引用元のChannelとGuild名を追加

--- src/views/date.ts
+++
@@ -1,4 +0,0 @@
-export function getDisplayDate(date: Date): string {
-  const formatter = Intl.DateTimeFormat("japanese", { dateStyle: "long", timeStyle: "long" });
-  return formatter.format(date);
-}
--- src/views/quote.ts
+++ src/views/quote.ts
@@ -1,9 +1,25 @@
-import { Message, MessageEmbed } from "discord.js";
-import { getDisplayDate } from "src/views/date";
+import { Message, MessageEmbed, TextChannel } from "discord.js";
 
 export type QuoteProps = {
   message: Message;
 };
+
+function getGuildName(message: Message): string | null {
+  const name = message.guild?.name;
+  return name ? name : null;
+}
+
+function getChannelName(message: Message): string | null {
+  const channel = message.channel;
+  if (!(channel instanceof TextChannel)) return null;
+  return channel.name;
+}
+
+function getChannelView(message: Message): string {
+  const guildName = getGuildName(message);
+  const channelName = getChannelName(message);
+  return [guildName, channelName].join("    ");
+}
 
 export function getQuoteEmbed({ message }: QuoteProps): MessageEmbed {
   const avatarURL = message.author.avatarURL();
@@ -11,7 +27,8 @@ export function getQuoteEmbed({ message }: QuoteProps): MessageEmbed {
     .setDescription(message.content)
     .setColor("GREEN")
     .setAuthor(message.author.username, avatarURL ? avatarURL : undefined)
-    .setFooter(getDisplayDate(message.createdAt));
+    .setFooter(getChannelView(message))
+    .setTimestamp(message.createdAt);
 
   message.attachments.map((attachment) => attachment.url).forEach((url) => embed.setImage(url));
   return embed;
