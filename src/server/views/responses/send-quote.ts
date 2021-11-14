2aa25437ffea8120e12059be0f8034b2058c4e6d
watano1168 <watano1168@gmail.com>
2021-11-14 21:34:09 +0900

    メッセージ引用機能を削除

--- src/server/views/responses/send-quote.ts
+++
@@ -1,6 +0,0 @@
-import { getQuoteEmbed, QuoteProps } from "src/server/views/quote";
-import { ReplyMessageOptions } from "discord.js";
-
-export function succeed({ quotes }: { quotes: QuoteProps[] }): ReplyMessageOptions {
-  return { embeds: quotes.map((quote) => getQuoteEmbed(quote)) };
-}
